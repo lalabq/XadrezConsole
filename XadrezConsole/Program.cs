4392e6ae55a7cc01d558d5c8ad0f53ef0aff7da6
Lavinea Sens <lavineasens@hotmail.com>
2021-02-21 20:02:14 -0300

    Criação da classe PosicaoXadrez

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -9,22 +9,9 @@ namespace XadrezConsole
         static void Main()
         {
 
-            try
-            {
-
-                Tabuleiro tabuleiro = new Tabuleiro();
-
-                tabuleiro.ColocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(0, 0));
-                tabuleiro.ColocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(0, 7));
-                tabuleiro.ColocarPeca(new Rei(Cor.Preta, tabuleiro), new Posicao(0, 3));
-
-                Tela.ImprimirTabuleiro(tabuleiro);
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            PosicaoXadrez posicaoXadrez = new PosicaoXadrez('A', 1);
+            Console.WriteLine(posicaoXadrez.ToString());
+            Console.WriteLine(posicaoXadrez.ConverterParaPosicao());
 
             Console.ReadLine();
 
---
+++ XadrezConsole/Xadrez/PosicaoXadrez.cs
@@ -0,0 +1,28 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    class PosicaoXadrez
+    {
+
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            Coluna = coluna;
+            Linha = linha;
+        }
+
+        public Posicao ConverterParaPosicao()
+        {
+            return new Posicao(8 - Linha, Coluna - 'A');
+        }
+
+        public override string ToString()
+        {
+            return "" + Coluna + Linha;
+        }
+
+    }
+}
